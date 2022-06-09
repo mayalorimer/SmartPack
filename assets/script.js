10a955680775001a59efdd93861028b46fbb8e50
Dasol Jessi Lee <dasol.jlee@gmail.com>
2022-06-09 13:37:18 -0700

    inprogress local storage

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// localStorage.setItem("itemsName", "value");
+// localStorage.getItem("itemsName")
+// localStorage.removeItem("itemsname")
+// localStorage.clear()
+
+// sessionStorage.setItem("itemname", "value") // saves the data in the item
+// sessionStorage.getItem("itemname")          // get the data in the item
+// sessionStorage.removeItem("itemname")       // removes the item
+// sessionStorage.clear()                      // removes all items
+
+// let name = localStorage.getItem('itemsName');
+// alert('Items' + itemsName);
+
