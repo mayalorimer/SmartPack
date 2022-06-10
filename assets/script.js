bbb97e0acefc4aae8a040aecff04b0515cb48b79
jesus villegas <jesusvicajr@hotmail.com>
2022-06-10 10:39:47 -0700

    API-WIP

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 var flightNum = "";
 var airLine = "";
 var flightDate = "";
-var latitude ="";
-var longitude = "";
+var latitudeNum ="";
+var longitudeNum = "";
 var flightUrl = `http://api.aviationstack.com/v1/flights?flight_number=${flightNum}&access_key=55013be62a6b251b0154473a727b8052`;
-var citiesUrl = "http://api.aviationstack.com/v1/cities?latitude&longitude&access_key=55013be62a6b251b0154473a727b8052"
+var citiesUrl = "http://api.aviationstack.com/v1/cities?latitude&longitude&access_key=55013be62a6b251b0154473a727b8052";
 var originalUrl = "http://api.aviationstack.com/v1/flights?access_key=55013be62a6b251b0154473a727b8052";
+var airlineUrl = "http://api.aviationstack.com/v1/airlines?airline_name&access_key=55013be62a6b251b0154473a727b8052";
 
 
 
 function getApi() {
     
-    fetch(citiesUrl)
+    fetch(airlineUrl)
       .then(function (response) {
         return response.json();
       })
@@ -19,6 +20,9 @@ function getApi() {
         console.log(data)
         console.log(data.data[0])
     displayFlight(data.data[0])
+    displayLongitude(data.data[0])
+    displayLatitude(data.data[0])
+    displayAirline(data.data[0])
       })
     }
      
@@ -31,10 +35,21 @@ function getApi() {
   }
 
   function displayLongitude(longitude){
+      longitude = longitude.longitude
       console.log (longitude)
-      
-  }
+    }
 
+  function displayLatitude(latitude){
+      latitude = latitude.latitude
+      console.log(latitude)
+    }
+   
+    function displayAirline(airline){
+        console.log(airline)
+        airLine = airline.airline_name  
+        console.log(airLine) 
+    
+    }
   
   
 
@@ -46,29 +61,29 @@ function getApi() {
 // temp = temperature from the forecast in farenheight 
 
 //var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
-var lat = 51.509865;
-var long = -0.118092;
-var Ktemp;
-var temp; 
+// var lat = 51.509865;
+// var long = -0.118092;
+// var Ktemp;
+// var temp; 
 
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
+// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
 
-function getApi(){
-    fetch(weatherUrl)
-        .then(function(response){
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            Ktemp = data.list[1].main.temp; 
-            temp = Math.floor((Ktemp - 273)*(9/5) + 32);
-            rain = data.list[1].pop; 
-            rain2 = data.list[1].weather.main;
-            console.log(temp);
-            console.log(rain);
-            console.log(rain2);
-        })
-}
+// function getApi(){
+//     fetch(weatherUrl)
+//         .then(function(response){
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//             Ktemp = data.list[1].main.temp; 
+//             temp = Math.floor((Ktemp - 273)*(9/5) + 32);
+//             rain = data.list[1].pop; 
+//             rain2 = data.list[1].weather.main;
+//             console.log(temp);
+//             console.log(rain);
+//             console.log(rain2);
+//         })
+// }
 
 
 getApi();
