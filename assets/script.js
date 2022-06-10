5c884ad6a3b1aeb8e7bae6d057596d09745e2e4b
jesus villegas <jesusvicajr@hotmail.com>
2022-06-10 13:17:53 -0700

    API fixing

--- assets/script.js
+++ assets/script.js
@@ -1,107 +1,154 @@
 var flightNum = "";
 var airLine = "";
 var flightDate = "";
-var latitudeNum ="";
-var longitudeNum = "";
+var city = "";
+var latitude ="";
+var longitude = "";
 var flightUrl = `http://api.aviationstack.com/v1/flights?flight_number=${flightNum}&access_key=55013be62a6b251b0154473a727b8052`;
-var citiesUrl = "http://api.aviationstack.com/v1/cities?latitude&longitude&access_key=55013be62a6b251b0154473a727b8052";
+var citiesUrl = "http://api.aviationstack.com/v1/cities?latitude&longitude&city_name&access_key=55013be62a6b251b0154473a727b8052";
 var originalUrl = "http://api.aviationstack.com/v1/flights?access_key=55013be62a6b251b0154473a727b8052";
 var airlineUrl = "http://api.aviationstack.com/v1/airlines?airline_name&access_key=55013be62a6b251b0154473a727b8052";
 
 
-
-function getApi() {
-    
-    fetch(airlineUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function(data){
-        console.log(data)
-        console.log(data.data[0])
-    displayFlight(data.data[0])
-    displayLongitude(data.data[0])
-    displayLatitude(data.data[0])
-    displayAirline(data.data[0])
-      })
-    }
-     
-  getApi();
-  
-  function displayFlight(flight){
-      console.log(flight)
-      flightDate = flight.flight_date
-      console.log(flightDate)
-  }
-
-  function displayLongitude(longitude){
-      longitude = longitude.longitude
-      console.log (longitude)
-    }
-
-  function displayLatitude(latitude){
-      latitude = latitude.latitude
-      console.log(latitude)
-    }
-   
-    function displayAirline(airline){
-        console.log(airline)
-        airLine = airline.airline_name  
-        console.log(airLine) 
-    
-    }
-
-const userQuizHeader = document.getElementById("userQuizHeader");
-const landingPage = document.getElementById("landing-page");
-const flightSearch = document.getElementById("flight-search");
-const flightForm = document.getElementById("flight-query");
-const userQuizHeader = document.getElementById("user-quiz-header");
-const userQuiz = document.getElementById("user-quiz");
-const question = document.getElementById("question");
-const answerA = document.getElementById("a");
-const answerB = document.getElementById("b");
-const answerC = document.getElementById("c");
-
-
-
-//var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
-var lat = 51.5880;
-var long = 19.7496;
-var Ktemp;
-var temp; 
-var weather;
-
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
-
-function weatherApi(){
-    fetch(weatherUrl)
-        .then(function(response){
+function getApi(){
+    fetch(flightUrl)
+        .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
-            Ktemp = data.list[1].main.temp; 
-            temp = Math.floor((Ktemp - 273)*(9/5) + 32); 
-            weather = data.list[1].weather[0].main;
-            console.log(temp);
-            console.log(weather);
+        .then(function (data){
+            console.log(data)
+            console.log(data.data[0])
+         })
+         //Date
+         .then(function displayFlight(flight){
+             console.log(flight)
+             flightDate = flight.flight_date
+             console.log(flightDate)
+            })
+          //City  
+    fetch(citiesUrl)
+        .then(function displayCity(cities){
+            console.log(cities)
+            city = cities.city_name
+            console.log(city)
         })
-}
+        .then(function displayLongitude(longitude){
+            longitude = longitude.longitude
+            console.log (longitude)
+          })
+        .then(function displayLatitude(latitude){
+            latitude = latitude.latitude
+            console.log(latitude)
+          })
+    fetch(airlineUrl)
+        .then( function displayAirline(airline){
+            console.log(airline)
+            airLine = airline.airline_name  
+            console.log(airLine)
+        })
+    }
+    getApi();
 
 
-// returns temp and weather which one of the possible returns is rain
-weatherApi();
+// function getApi() {
+    
+//     fetch(flightUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function(data){
+//         console.log(data)
+//         console.log(data.data[0])
+//     displayFlight(data.data[0])
+//     displayCity(data.data[0])
+//     displayLongitude(data.data[0])
+//     displayLatitude(data.data[0])
+//     displayAirline(data.data[0])
+//       })
+//     }
+     
+//   getApi();
+  
+//   function displayFlight(flight){
+//       console.log(flight)
+//       flightDate = flight.flight_date
+//       console.log(flightDate)
+//   }
 
-// array to hold the items in the packing list
-var packingList = [];
-//if the weather forcasts rain
-if(weather == "Rain") {
-    packingList.push("rain jacket"); 
-}
-else {
-    packingList.push("jacket");
-}
-console.log(packingList);
+//   function displayCity(cities){
+//       console.log(cities)
+//       city = cities.city_name
+//       console.log(city)
+//     }
+
+//   function displayLongitude(longitude){
+//       longitude = longitude.longitude
+//       console.log (longitude)
+//     }
+
+//   function displayLatitude(latitude){
+//       latitude = latitude.latitude
+//       console.log(latitude)
+//     }
+   
+//     function displayAirline(airline){
+//         console.log(airline)
+//         airLine = airline.airline_name  
+//         console.log(airLine) 
+    
+//     }
+
+// const userQuizHeader = document.getElementById("userQuizHeader");//////
+// const landingPage = document.getElementById("landing-page");
+// const flightSearch = document.getElementById("flight-search");
+// const flightForm = document.getElementById("flight-query");
+// const userQuizHeader = document.getElementById("user-quiz-header");///////
+// const userQuiz = document.getElementById("user-quiz");
+// const question = document.getElementById("question");
+// const answerA = document.getElementById("a");
+// const answerB = document.getElementById("b");
+// const answerC = document.getElementById("c");
+
+
+
+// //var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
+// var lat = 51.5880;
+// var long = 19.7496;
+// var Ktemp;
+// var temp; 
+// var weather;
+
+// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
+
+// function weatherApi(){
+//     fetch(weatherUrl)
+//         .then(function(response){
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//             Ktemp = data.list[1].main.temp; 
+//             temp = Math.floor((Ktemp - 273)*(9/5) + 32); 
+//             weather = data.list[1].weather[0].main;
+//             console.log(temp);
+//             console.log(weather);
+//         })
+// }
+
+
+// // returns temp and weather which one of the possible returns is rain
+// weatherApi();
+
+// // array to hold the items in the packing list
+// var packingList = [];
+// //if the weather forcasts rain
+// if(weather == "Rain") {
+//     packingList.push("rain jacket"); 
+// }
+// else {
+//     packingList.push("jacket");
+// }
+// console.log(packingList);
 
 /*
 //if you run hot
@@ -155,40 +202,40 @@ if (both relax and push){
 
 // Custom list
 
-$("#custom-item-add").click( function() {
-	customItemAdd();
-	$("body, html").scrollTop( $("#custom-items").offset().top );
-}); 
+// $("#custom-item-add").click( function() {
+// 	customItemAdd();
+// 	$("body, html").scrollTop( $("#custom-items").offset().top );
+// }); 
 
-// Add custom item to the list of generated items.
-function customItemAdd() {
+// // Add custom item to the list of generated items.
+// function customItemAdd() {
 
-	// prevent default
-	event.preventDefault();
+// 	// prevent default
+// 	event.preventDefault();
 
-	// Show the custom-items wrap div 
-	if ( !$("#custom-items").show() && $("#custom-item-value").val() ) {
-		$("#custom-items").show(200);
-	}
+// 	// Show the custom-items wrap div 
+// 	if ( !$("#custom-items").show() && $("#custom-item-value").val() ) {
+// 		$("#custom-items").show(200);
+// 	}
 
-	// make the div glow 
-	$("#custom-items").css("boxShadow", "0 0 5rem rgba(255,255,0,0.7)");
-	$("#custom-items").delay(300).queue(function(){
-		$(this).css("boxShadow", "0 0 0rem rgba(255,255,0,0.7)").dequeue();
-	});
+// 	// make the div glow 
+// 	$("#custom-items").css("boxShadow", "0 0 5rem rgba(255,255,0,0.7)");
+// 	$("#custom-items").delay(300).queue(function(){
+// 		$(this).css("boxShadow", "0 0 0rem rgba(255,255,0,0.7)").dequeue();
+// 	});
 
-	// create a new subitem in that div, give it the value from the custom-item-value box. Clear that box. Check to see if custom-items is all checked off (because it isn't, because we JUST added something, so this will reset it)
-	if ( $("#custom-item-value").val() ) {
-		customEl = document.createElement("p");
-		$(customEl).addClass("subitem");
-		$(customEl).text( $("#custom-item-value").val() );
-		document.querySelector("#custom-items .value").appendChild( customEl );
-		$("#custom-item-value").val('');
+// 	// create a new subitem in that div, give it the value from the custom-item-value box. Clear that box. Check to see if custom-items is all checked off (because it isn't, because we JUST added something, so this will reset it)
+// 	if ( $("#custom-item-value").val() ) {
+// 		customEl = document.createElement("p");
+// 		$(customEl).addClass("subitem");
+// 		$(customEl).text( $("#custom-item-value").val() );
+// 		document.querySelector("#custom-items .value").appendChild( customEl );
+// 		$("#custom-item-value").val('');
 		
-		checkDone( $(customEl) );
-	}
+// 		checkDone( $(customEl) );
+// 	}
 
-}
+// }
 
 
 // localStorage.setItem("itemsName", "value");
