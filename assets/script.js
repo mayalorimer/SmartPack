4f31724a01d841eceba8d1f86bd2a916d47ee72f
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-13 09:31:37 -0700

    Deleted repeated items

--- assets/script.js
+++ assets/script.js
@@ -10,18 +10,6 @@ var airline = document.getElementById("#airline");
 var flightNumber = document.getElementById("#flight-number");
 
 
-
-
-
-//var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
-var lat = 51.5880;
-var long = 19.7496;
-var Ktemp;
-var temp; 
-var weather;
-
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
-
 var flightNum = "";
 var airLine = "";
 var flightDate = "";
@@ -135,60 +123,14 @@ function getApi(){
 
 //     }
 
-// const userQuizHeader = document.getElementById("userQuizHeader");//////
-// const landingPage = document.getElementById("landing-page");
-// const flightSearch = document.getElementById("flight-search");
-// const flightForm = document.getElementById("flight-query");
-// const userQuizHeader = document.getElementById("user-quiz-header");///////
-// const userQuiz = document.getElementById("user-quiz");
-// const question = document.getElementById("question");
-// const answerA = document.getElementById("a");
-// const answerB = document.getElementById("b");
-// const answerC = document.getElementById("c");
+//var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
+var lat = 51.5880;
+var long = 19.7496;
+var Ktemp;
+var temp; 
+var weather;
 
-
-
-// //var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
-// var lat = 51.5880;
-// var long = 19.7496;
-// var Ktemp;
-// var temp; 
-// var weather;
-
-// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
-
-// function weatherApi(){
-//     fetch(weatherUrl)
-//         .then(function(response){
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//             Ktemp = data.list[1].main.temp; 
-//             temp = Math.floor((Ktemp - 273)*(9/5) + 32); 
-//             weather = data.list[1].weather[0].main;
-//             console.log(temp);
-//             console.log(weather);
-//         })
-// }
-
-
-// // returns temp and weather which one of the possible returns is rain
-// weatherApi();
-
-// // array to hold the items in the packing list
-// var packingList = [];
-// //if the weather forcasts rain
-// if(weather == "Rain") {
-//     packingList.push("rain jacket"); 
-// }
-// else {
-//     packingList.push("jacket");
-// }
-// console.log(packingList);
-
-    }
-
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
 
 
 function weatherApi(){
@@ -209,8 +151,6 @@ function weatherApi(){
 
 // returns temp and weather which one of the possible returns is rain
 weatherApi();
-
-createElements(dataObject);
 	
 
 // array to hold the items in the packing list
@@ -225,8 +165,8 @@ else {
 console.log(packingList);
 
 
-
-//if (runHot) {
+//logic statements to create the packing list
+if (hot || inbetween) {
     if (temp < 50) {
         packingList.push("long pants", "long sleeve shirt", "sweater", "winter jacket", "closed toe shoes", "base layer shirt");
     }
@@ -261,15 +201,14 @@ if (business) {
     packingList.push("dress shoes", "slacks", "blazer", "formal top");
 }
 
-if (keepItPushing){
-    packingList.push("sneakers", "athletic top", "athletic bottoms");
-}
 
-if (relax) {
+if (beach-bum) {
     packingList.push("bathing suit", "flip flops");
 }
-
-if (bothRelaxAndPush){ // added it together -jess
+else if (keepItPushing){
+  packingList.push("sneakers", "athletic top", "athletic bottoms");
+}
+else { // added it together -jess
     packingList.push("sneakers", "flip flops", "athletic outfit", "bathing suit");
 }
 
